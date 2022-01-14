dfa849fbc9a46745368539e7f7c671ca11f5526e
amplil <aikotobahaaburakatabura@gmail.com>
2022-01-15 02:06:10 +0900

    webdriver_managerの使用

--- ui_show.py
+++ ui_show.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from time import sleep
 import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ['enable-automation'])
 
 # プラウザ起動（Chrome）
-driver = webdriver.Chrome(r'chromedriver.exe', chrome_options=options) # chromedriver.exeを使う
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options) # chromedriver.exeを使う
 
 # リストからURLをひとつづつ処理
 print('file:///{}/tmp.png'.format(os.getcwd()))
